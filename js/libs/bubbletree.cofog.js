93b28cf8f47ece5fff00a980d939b25b70703e11
Tryggvi Bjorgvinsson <tryggvi.bjorgvinsson@okfn.org>
2013-07-02 15:41:46 +0000

    Update bubbletree cofogs (the data is really messed up)

--- js/libs/bubbletree.cofog.js
+++ js/libs/bubbletree.cofog.js
@@ -24,7 +24,7 @@ BubbleTree.Styles.Cofog1 = {
     'aktivnosti-obrazovanja': { icon: 'education.svg', color: '#42928F' },
     // Military
     'aktivnosti-odbrane': { icon: 'defence.svg', color: '#D32645' },
-    'odbrana': { icon: 'defence.svg', color: '#D32645' },
+    'odbrana': { icon: 'EC.svg', color: '#D32645' },
     // Running government
     'opste-javne-usluge': { icon: 'government-uk.svg', color: '#CD531C' },
     'opste-javne-sluzbe': { icon: 'government-uk.svg', color: '#CD531C' },
@@ -56,96 +56,87 @@ BubbleTree.Styles.Cofog1 = {
 }
 
 BubbleTree.Styles.Cofog2 = {
-	'01.1': { icon: 'legislative.svg' },
-	'01.2': { icon: 'aid.svg' },
-	'01.3': { icon: 'misc-services.svg' },
-	'01.7': { icon: 'public-debt.svg' },
-	
-	'02.1': { icon: 'military.svg' },
-	'02.2': { icon: 'civil-defence.svg' },
-	'02.3': { icon: 'foreign-military-aid.svg' },
-	'02.4': { icon: 'defence-research.svg' },
-	'02.5': { icon: 'defence-admin.svg' },
-	
-	'03.1': { icon: 'police.svg' },
-	'03.2': { icon: 'fire-brigade.svg' },
-	'03.3': { icon: 'courts.svg' },
-	'03.4': { icon: 'prisons.svg' },
-	'03.5': { icon: 'rd-order-safety.svg' },
-	'03.6': { icon: 'admin-order-safety.svg' },
-	
-	'04.1': { icon: 'social-systems.svg' },
-	'04.2': { icon: 'farms.svg' },
-	'04.3': { icon: 'energy.svg' },
-	'04.4': { icon: 'manufactoring-construction.svg' },
-	'04.5': { icon: 'transport.svg' },
-	'04.6': { icon: 'police.svg' },
-	'04.7': { icon: 'police.svg' },
-	//'04.8': { icon: 'rd-eco.svg' },
-	'04.9': { icon: 'police.svg' },
-	
-	'05.1': { icon: 'waste.svg' },
-	'05.2': { icon: 'toilet.svg' },
-	'05.3': { icon: 'pollution.svg' },
-	'05.4': { icon: 'tree.svg' },
-	'05.6': { icon: 'environment-admin.svg' },
-	
-	'06.1': { icon: 'housing.svg' },		
-	'06.2': { icon: 'community.svg' },
-	'06.3': { icon: 'water.svg' },
-	'06.4': { icon: 'street-lights.svg' },
-	
-	'07.1': { icon: 'medical-supplies.svg' },				
-	'07.2': { icon: 'health.svg' },
-	'07.3': { icon: 'hospital.svg' },
-	
-	'08.2': { icon: 'culture.svg' },					
-	'08.1': { icon: 'sports.svg' },	
-	'08.3': { icon: 'media.svg' },	
-	
-	'10.1': { icon: 'helping-others.svg' },
-	'10.2': { icon: 'old-age.svg' },
-	'10.4': { icon: 'family.svg' },
-	'10.7': { icon: 'family2.svg' }
-
+    'parlamentarna-skupstina-bosne-i-hercegovine': { icon: 'legislative.svg' },
+    'centralna-izborna-komisija-bih': { icon: 'legislative.svg' },
+    'direkcija-za-ekonomsko-planiranje': { icon: 'planning.svg' },
+    'rezervisanja': { icon: 'pig.svg' },
+    'predsjednistvo-bih': { icon: 'president.svg' }, // Need to create
+    'sluzba-za-zajednicke-poslove-institucija-bosne-i-hercegovine': { icon: 'misc-services.svg' },
+    'agencija-za-identifikacione-dokumente-evidenciju-i-razmjenu-podataka-bih': { icon: 'admin.svg' },
+    'ministarstvo-vanjskih-poslova-bosne-i-hercegovine': { icon: 'worldmap.svg' },
+    'direktni-transferi-sa-jrt' : { icon: 'financial-admin.svg' },
+    'ured-za-razmatranje-zalbi-po-javnim-nabavkama': { icon: 'financial-admin.svg' },
+    'servisiranje-spoljnog-duga': { icon: 'public-debt.svg' },
+    'ostalo': { icon: 'money.svg' },
+    'ministarstvo-finansija-i-trezora-bih': { icon: 'money.svg' },
+    'agencija-za-javne-nabavke-bih': { icon: 'money.svg' },
+    'obavjestajno-sigurnosna-agencija-bosne-i-hercegovine' : { icon: 'intelligence-agency.svg' }, // Need to create
+    'ministarstvo-odbrane-bih': { icon: 'military.svg' },
+    'drzavna-agencija-za-istrage-i-zastitu': { icon: 'police.svg' },
+    'granicna-policija-bosne-i-hercegovine': { icon: 'police2.svg' },
+    'sud-bosne-i-hercegovine': { icon: 'courts.svg' },
+    'ministarstvo-pravde-bih': { icon: 'courts.svg' },
+    'visoko-sudsko-i-tuzilacko-vijece-bosne-i-hercegovine': { icon: 'courts.svg' },
+    'tuzilastvo-bosne-i-hercegovine': { icon: 'courts.svg' },
+    'direkcija-za-koordinaciju-policijskih-tijela': { icon: 'admin-order-safety.svg' },
+    'uprava-za-indirektno-oporezivanje-bosne-i-hercegovine': { icon: 'social-systems.svg' },
+    'ministarstvo-vanjske-trgovine-i-ekonomskih-odnosa-bosne-i-hercegovine': { icon: 'import-export.svg' }, // Import export
+    'ministarstvo-komunikacija-i-prometa-bosne-i-hercegovine': { icon: 'communication-transport.svg'}, // Need to create
+    'direkcija-za-civilno-vazduhoplovstvo-bih': { 'icon': 'airplane.svg' },
+    'agencija-za-promociju-stranih-investicija-u-bih': { icon: 'economic-aid.svg' },
+    'konkurencijsko-vijece-bosne-i-hercegovine': { icon: 'economic-aid.svg' }, // Need update?
+    'agencija-za-postanski-promet-bih': { icon: 'postal-service.svg' }, // Need to create
+    'uprava-bih-za-zastitu-zdravlja-bilja': { icon: 'tree.svg' },
+    'komisija-za-koncesije-bih': { icon: 'manufactoring-construction.svg' },
+    'agencija-za-sigurnost-hrane-bih': { icon: 'food.svg' }, // Need to create
+    'komisija-za-ocuvanje-nacionalnih-spomenika': { icon: 'heritage.svg' }, // Need to create
+    'memorijalni-centar-srebrenica-potocari': { icon : 'potocari.svg' }, // Need to create
+    'agencija-za-lijekove-i-medicinska-sredstva-bosne-i-hercegovine': { icon: 'medical-supplies.svg' },				
+    'ured-za-veterinarstvo-bih': { icon: 'animal.svg' }, // Need to create
+    'agencija-za-forenzicka-ispitivanja-i-vjestacenja': { icon: 'microscope.svg' },	
+    'agencija-za-antidoping-kontrolu': { icon: 'sports.svg' },	
+    'agencija-za-predskolsko-osnovno-i-srednje-obrazovanje-bih': { icon: 'schools.svg' },
+    'agencija-za-razvoj-visokog-obrazovanja-i-osiguranje-kvaliteta': { icon: 'education.svg' },
+    'centar-za-informisanje-i-priznavanje-dokumenata-iz-oblasti-visokog-obrazovanja': { icon : 'education.svg'}, // Need to update? (umsjón)
+    'ministarstvo-civilnih-poslova': { icon: 'family.svg' },
+    'fond-za-povratak-bih': { icon: 'family.svg' },
+    'minstarstvo-za-ljudska-prava-i-izbjeglice-bih': { icon: 'family2.svg' },
+    'sluzba-za-poslove-sa-strancima-bosne-i-hercegovine': { icon: 'world-family.svg' } // Need to create
 };
 
-BubbleTree.Styles.Cofog3 = {
+var titles = {
+    'regulatorna-agencija-za-komunikacije': { icon: 'media.svg' },
+    'agencija-za-statistiku-bih': { icon: 'statistics.svg' }, // Need to create
+    'centar-za-uklanjanje-mina-u-bosni-i-hercegovini-bhmac': { icon: 'no-bomb.svg' }, // Need to create
+    'ured-koordinatora-za-reformu-javen-uprave': { icon: 'admin.svg' },
+    'ustavni-sud-bosne-i-hercegovine': { icon: 'court.svg' },
+    'agencija-za-lijekove-i-medicinska-sredstva-bosne-i-hercegovine': { icon: 'medical-supplies.svg' },
+    'agencija-za-skolovanje-i-strucno-osposobljavanje-kadrova': { icon: 'schools.svg' },
+    'direkcija-za-europske-integracije': { icon: 'EC.svg' },
+    'generalni-sekretarijat-vijeca-ministara-bosne-i-hercegovine': { icon: 'admin.svg' },
+    'ured-za-reviziju-institucija-bih': { icon: 'financial-admin.svg' },
+    'institut-za-nestala-lica-bih': { icon: 'missing-person.svg' }, // Need to create
+    'jedinica-za-implementaciju-projekta-izgradnje-zavoda-za-izvrsenje-kaznenih-sankcija-pritvora-i-drugih-mjera-bih': { icon: 'prisons.svg' },
+    'institut-za-mjeriteljstvo-bih': { icon: 'measurements.svg' }, // Need to create
+    'institucija-ombdusmana-za-ljudska-prava-bih': { icon: 'unemployment.svg' },
+    'institut-za-intelektualno-vlasnistvo-bih': { icon: 'books.svg' },
+    'agencija-za-policijsku-podrsku': { icon: 'police.svg' },
+    'institut-za-standardizaciju-bih': { icon: 'research.svg' }, // Need to create
+    'agencija-za-drzavnu-sluzbu-bih': { icon: 'misc-services.svg' },
+    'agencija-za-rad-i-zaposljavanje-bih': { icon: 'labour.svg' },
+    'agencija-za-zastitu-licnih-podataka-u-bih': { icon: 'unemployment.svg' },
+    'instut-za-akreditiranje-bih': { icon: 'measurements.svg'}, // Need to create
+    'pravobranilastvo-bosne-i-hercegovine': { icon: 'court.svg' },
+    'drzavna-regulatorna-agencija-za-radijacijsku-i-nuklearnu-bezbjednost-u-bih': { icon: 'nuclear.svg' },
+    'agencija-za-prevenciju-korupcije-i-koordinaciju-borbe-protiv-korupcije': { icon: 'corruption.svg' }, // Need to create
+    'agencija-za-nadzor-nad-trzistem-bih': { icon: 'money.svg' }, // Need to create?
+    'arhiv-bih': { icon: 'file-cabinet.svg' }, // Need to create
+    'ured-za-zakonodavstvo': { icon: 'court.svg'},
+    'agencija-za-osiguranje-u-bih': { icon: 'insurance.svg' }, // Need to create
+    'ured-za-harmonizaciju-i-koordinaciju-sistema-placanja-u-poljoprivredi-i-ruralnom-razvoju': { icon: 'farms.svg' },
+    'centralna-harmonizacijska-jedinica': { icon: 'misc-services.svg' },
+    'odbor-drzavne-sluzbe-za-zalbe': { icon: 'admin.svg' },
+    'institucija-ombdusmana-za-zastitu-potrosaca-u-bih': { icon: 'economic-aid.svg' }
+};
 
-	'01.1.1': { icon: 'legislative.svg' },
-	'01.1.2': { icon: 'pig.svg' },
-	'01.1.3': { icon: 'worldmap.svg' },
-	'01.2.1': { icon: 'aid-developing-countries.svg' },
-	'01.2.2': { icon: 'economic-aid.svg' },
-	'01.3.1': { icon: 'human-resources.svg' },
-	'01.3.2': { icon: 'planning.svg' },
-	'01.3.3': { icon: 'research.svg' },
-
-	'04.1.1': { icon: 'social-systems.svg' },
-	'04.1.2': { icon: 'labour.svg' },					
-	'04.2.1': { icon: 'farms.svg' },
-	'04.2.2': { icon: 'forest.svg' },
-	'04.2.3': { icon: 'fishing.svg' },				
-	'04.3.1': { icon: 'coal.svg' },
-	'04.3.2': { icon: 'petrol.svg' },
-	'04.3.3': { icon: 'nuclear.svg' },
-	'04.3.4': { icon: 'fuel.svg' },
-	'04.3.5': { icon: 'electricity.svg' },
-	'04.3.6': { icon: 'wind.svg' },
-	'04.5.1': { icon: 'car.svg' },
-	'04.5.2': { icon: 'anchor.svg' },
-	'04.5.3': { icon: 'railways.svg' },
-	'04.5.4': { icon: 'airplane.svg' },
-
-	'07.1.1': { icon: 'medical-supplies.svg' },				
-	'07.1.2': { icon: 'other-medical-products.svg' },				
-	'07.1.3': { icon: 'wheelchair.svg' },		
-	'07.2.1': { icon: 'health.svg' },
-	'07.2.2': { icon: 'microscope.svg' },
-	'07.2.2': { icon: 'dental.svg' },
-	'07.3.1': { icon: 'hospital.svg' },
-	'07.3.2': { icon: 'hospital-specialized.svg' },
-	'07.3.2': { icon: 'dental.svg' },
-
-	'10.1.2': { icon: 'helping-others.svg' }
-}
+BubbleTree.Styles.Title = $.extend({}, BubbleTree.Styles.Cofog2, titles)
